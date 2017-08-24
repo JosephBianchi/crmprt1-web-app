d2e5ced95c60960559617dc5a3fd16e10e427b6e
Joe Bianchi <joseph.bianchi3@gmail.com>
2017-08-23 22:17:38 -0400

    finished prt1 crm web app

--- crm.rb
+++ crm.rb
@@ -4,3 +4,18 @@ require 'sinatra'
 get '/index' do
   erb :index
 end
+
+get '/contacts' do
+  @contacts = Contact.all
+  erb :contacts
+end
+
+
+
+
+
+
+
+after do
+  ActiveRecord::Base.connection.close
+end
